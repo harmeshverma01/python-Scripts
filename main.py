c8fa55a79e147104d1490286ec74bfc082a87a33
harmeshverma01 <harmeshv@excellencetechnologies.in>
2022-08-22 13:24:54 +0530

    Working on add to scrape products save data from each page and save in json format

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-
 import json
 from util.utils import product_details, html_content, get_next_page
 url = "https://www.flipkart.com/search?q=mobiles"
 
-
-
-
 soup = html_content(url)
-divs = soup.find_all("div",{"class": "_2kHMtA"})
 
-list_of_product = product_details(divs, list_of_product=[])
-
-
-nextpage = get_next_page(soup)
 
 while True:
-    soup = product_details(divs, list_of_product)
-    url = get_next_page(soup)
+    divs = soup.find_all("div",{"class": "_2kHMtA"})
+    list_of_product = product_details(divs, list_of_product=[])
+    url = get_next_page(url)
     if not url:
         break
+    soup = html_content(url)
     print(url)
 
-with open('data.json', 'w')as f:
-    json.dump(list_of_product, f) 
+    with open('data.json', 'a')as f:
+        json.dump(list_of_product, f) 
 
-# with open('data.json', 'w')as f:
-#     json_data = json.dumps(list_of_product)
-#     print(json_data, 'abcd')
-#     json.dump(json_data, f)
-# #json.load([list_of_product])        
+      
         
