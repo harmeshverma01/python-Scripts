ba598017f68a2c57bc550bdb9c93f0c6285fd02f
harmeshverma01 <harmeshv@excellencetechnologies.in>
2022-09-05 16:10:52 +0530

    Add some contents

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
-from db import get_database
+# from db import get_database
 from util.utils import product_details, html_content, get_next_page
 
-db = get_database()
-product_coll = db['products']
+# db = get_database()
+# product_coll = db['products']
 
 url = "https://www.flipkart.com/search?q=mobiles"
 
@@ -13,13 +13,13 @@ soup = html_content(url)
 while True:
     divs = soup.find_all("div",{"class": "_2kHMtA"})
     list_of_product = product_details(divs, list_of_product=[])
-    if list_of_product:
-        product_coll.insert_many(list_of_product)
+    # if list_of_product:
+    #     product_coll.insert_many(list_of_product)
     url = get_next_page(url)
     if not url:
         break
     soup = html_content(url)
     
-    # with open('data.json', 'a')as f:
-    #     json.dump(collenction_data, f)
+    with open('data.json', 'a')as f:
+        json.dump(list_of_product, f)
         
